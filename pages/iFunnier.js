d6fafd61190f6ddc3963e1e793b8e8aed9dbe096
Eamon Tracey <eamontracey@Eamons-MacBook-Air-3.local>
2020-12-18 01:17:55 -0500

    upload ifunnier

---
+++ pages/iFunnier.js
@@ -0,0 +1,36 @@
+import Depiction from "../components/Depiction"
+
+export default function iFunnier() {
+	return (
+		<Depiction
+			name="iFunnier"
+			github="https://github.com/EamonTracey/iFunnier"
+			subtitle="Block ads, save any content (no watermark), and more on iFunny!"
+		>
+			
+			<p>
+			iFunnier enhances your iFunny experience!
+			</p>
+
+            <p>Features:</p>
+			<ul>
+				<li>
+					Block all ad requests and popovers
+				</li>
+				<li>
+                    Save photos without the iFunny watermark
+				</li>
+				<li>
+					Save videos
+				</li>
+                <li>
+					Easily access your bearer authorzation token
+				</li>
+                <li>
+					Beautiful preferences pane
+				</li>
+			</ul>
+
+		</Depiction>
+	)
+}
