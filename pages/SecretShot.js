f1bfee629a2cca26b0acfe9168b32e8024df62b4
EamonTracey <etracey21@regis.org>
2021-02-25 10:53:00 -0500

    upload SecretShot

---
+++ pages/SecretShot.js
@@ -0,0 +1,17 @@
+import Depiction from "../components/Depiction"
+
+export default function SecretShot() {
+	return (
+		<Depiction
+			name="SecretShot"
+			github="https://github.com/EamonTracey/SecretShot"
+			subtitle="Block Snapchat screenshot and screen recording detection"
+		>
+			
+			<p>
+			SecretShot prevents Snapchat from detecting when you take screenshots or screen recordings. When enabled, users whose snaps you screenshot or screen record will not be notified. SecretShot does not hook Snapchat classes, meaning Snapchat will not detect any tweak injection. Thus, you will not be banned.
+			</p>
+
+		</Depiction>
+	)
+}
